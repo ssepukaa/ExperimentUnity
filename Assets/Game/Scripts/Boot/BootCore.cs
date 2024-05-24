8aa8cf36f152083b6ccb041c28b237efefdfaf2b
vamart <vamartem13@gmail.com>
2024-05-24 12:45:35 +0300

    init

---
+++ Assets/Game/Scripts/Boot/BootCore.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Assets.Game.Scripts.Boot {
+    public class BootCore : MonoBehaviour {
+        public static BootCore Instance;
+
+        private void Awake() {
+            if (Instance == null) {
+                Instance = this;
+                DontDestroyOnLoad(gameObject);
+            } else {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Game/Scripts/Cores/Game.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Assets.Game.Scripts.Cores {
+    public class Game : MonoBehaviour {
+        public static Game Instance;
+
+        private void Awake() {
+            if (Instance == null) {
+                Instance = this;
+                DontDestroyOnLoad(gameObject);
+            } else {
+                Destroy(gameObject);
+            }
+
+        }
+
+    }
+}
+
